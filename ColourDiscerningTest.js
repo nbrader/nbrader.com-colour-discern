103d0051fd444e75445153d251b828d0a58d67ce
nbrader <nbrader+github@gmail.com>
2023-09-18 21:12:41 +0100

    Revert "Simplified function"
    This reverts commit 19bed278647d667a5b15d0236c2ccd91d3ba806e.
    
    That commit had made the colour B values non-integer (usually 0.2 more than an integer though sometimes an integer) which affected things like making them appear white in the visualisation.

--- ColourDiscerningTest.js
+++ ColourDiscerningTest.js
@@ -398,9 +398,10 @@ function setColors() {
 
 function generateColorWithDeviation(baseColor, actualDeviation) {
     return baseColor.map(channel => {
-        let deviation = Math.floor(Math.random() * (2 * actualDeviation + 1)) - actualDeviation;
-        let adjustedChannel = channel + deviation;
-        return Math.max(0, Math.min(adjustedChannel, 255));
+        let deviation = Math.floor(Math.random() * (actualDeviation + 1));
+        let adjustedChannel = (Math.random() > 0.5) ? channel + deviation : channel - deviation;
+        adjustedChannel = Math.max(0, Math.min(adjustedChannel, 255));
+        return adjustedChannel;
     });
 }
 
